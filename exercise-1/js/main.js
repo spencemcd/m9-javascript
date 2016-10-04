8fd07fd25f6dfa75085ab00bbb30918c0fa381f6
Michael Freeman <michael.k.freeman@gmail.com>
2016-10-04 12:38:02 -0700

    Added missing semi-colons

--- exercise-1/js/main.js
+++ exercise-1/js/main.js
@@ -10,14 +10,14 @@ console.log(str.length);
 var STR = str.toUpperCase();
 
 // Use the "split" method create a new variable `words` that is an array of the component words of `str`
-var words = str.split(' ')
+var words = str.split(' ');
 
 // Use the length property to see how many words are in your `words` array
 console.log(words.length);
 
 // Write a function that returns the number of words in a string
 var numWords = function(word) {
-  return(word.split(' ').length)
+    return (word.split(' ').length);
 };
 
 // Pass a string to your function to determine that it works
@@ -25,7 +25,7 @@ console.log(numWords('This is a test.'));
 
 // Write a function to test if a string contains a word, and returns a boolean value.
 var contains = function(str, word) {
-  return(str.indexOf(word) != -1)
+    return (str.indexOf(word) != -1);
 };
 
 // Pass a phrase and a string to your function to determine that it works
--- exercise-2/js/main.js
+++ exercise-2/js/main.js
@@ -1,39 +1,46 @@
 // Main JavaScript File
 
 // Array to work with
-var arr1 = [1,2,3,4,5];
+var arr1 = [1, 2, 3, 4, 5];
 
 // Write a function to subtract one from a value
 var subtractOne = function(value) {
-	return(value - 1)
+    return (value - 1);
 };
 
 // Pass each value to the subtractOne function by using the map functionality
 var oneLess = arr1.map(subtractOne);
 
 // Create a array equal to arr1 times two by using the inline "map" funcitonality
-var timesTwo = arr1.map(function(d){return d*2 });
+var timesTwo = arr1.map(function(d) {
+    return d * 2;
+});
 
 // Array of strings
 var names = ['Tim', 'Nick', 'Grant'];
 
 // Write an introduction function that takes an argument and returns "hello, I'm " + argument
 var intro = function(name) {
-	return("hello, I'm " + name)
+    return ("hello, I'm " + name);
 };
 
 // Create an array of introductions for each name
-var intros = names.map(intro)
+var intros = names.map(intro);
 
 // Array of objects
-var people = [
-	{name:'Tim', age:22},
-	{name:'Jan', age:12},
-	{name:'Isabel', age:43}
-];
+var people = [{
+    name: 'Tim',
+    age: 22
+}, {
+    name: 'Jan',
+    age: 12
+}, {
+    name: 'Isabel',
+    age: 43
+}];
 
 // Use the `.forEach` method to create a new property `intro` for each object in your people array
 // The property should say "My name is NAME and I'm AGE years old"
-people.forEach(function(d){
-	d.intro = "My name is " + d.name + " and I'm " + d.age + " years old."
+people.forEach(function(d) {
+    d.intro = "My name is " + d.name + " and I'm " + d.age + " years old.";
 });
