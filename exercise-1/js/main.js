1777ce552926d6cb61120524c4f71a4b96673538
Spencer McDonald <mcdonaldspence@hotmail.com>
2017-07-30 15:28:49 -0700

    Finished exercise 1

--- exercise-1/js/main.js
+++ exercise-1/js/main.js
@@ -4,17 +4,24 @@
 var str = "this is a sentence";
 
 // Use the length property to see how many characters are in your `str` variable
-
+var length = str.length;
 // Use the `toUpperCase` method to create a new variable `STR` that has the `str` value, but all UPPPERCASE.
-
+var STR = str.toUpperCase();
 // Use the "split" method create a new variable `words` that is an array of the component words of `str`
-
+var words = str.split(" ");
 // Use the length property to see how many words are in your `words` array
-
+var word_length = words.length;
 // Write a function that returns the number of words in a string
-
+function Words(w) {
+	var arr = w.split(" ");
+	return arr.length;
+}
 // Pass a string to your function to determine that it works
-
+var ftest = Words(str);
 // Write a function to test if a string contains a word, and returns a boolean value.
-
+function ContainWord(str, w) {
+	var temp = str.indexOf(w);
+	return (temp != -1);
+}
 // Pass a phrase and a string to your function to determine that it works
+var works = ContainWord("Hello How are You", "How");
